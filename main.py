fc6029ae899da9fe6d5f0608c03d5e6b2aee0eb9
Bel Downes <belindadownes@live.com.au>
2023-04-30 22:51:10 +1000

    commit from laptop

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-print ("This is a place keeper")
-print ("this project will tag different parts of speech")
+print("This is a place keeper")
+print("this project will tag different parts of speech")
+print("I'm just test to see if I can edit this on my second computer")
