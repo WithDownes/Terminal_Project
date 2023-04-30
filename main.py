7bdcf322707d4bbd219aa094a09187022a4cf869
Bel Downes <117497785+WithDownes@users.noreply.github.com>
2023-05-01 09:23:04 +1000

    template

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print ("This is a place keeper")
-print ("this project will tag different parts of speech")
+import nltk
+from nltk.tokenize import word_tokenize
+
+# Prompt the user to enter a sentence
+sentence = input("Enter a sentence: ")
+
+# Tokenize the sentence into words
+words = word_tokenize(sentence)
+
+# Use NLTK's part-of-speech (POS) tagger to tag each word with its corresponding POS tag
+pos_tags = nltk.pos_tag(words)
+
+# Print the tagged words and their POS tags
+print(pos_tags)
