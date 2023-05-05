94a7f9109c1f578150a55b4a60e9ade8270d59fe
Bel Downes <117497785+WithDownes@users.noreply.github.com>
2023-05-05 15:34:37 +1000

    tidy

--- main.py
+++ main.py
@@ -34,17 +34,17 @@ while True:
         print("Option 1 selected.")
         # Part of speech tagger
 
-            # Prompt the user to enter a sentence
-            sentence = input("Enter a sentence: ")
+        # Prompt the user to enter a sentence
+        sentence = input("Enter a sentence: ")
 
-            # Tokenize the sentence into words
-            words = word_tokenize(sentence)
+        # Tokenize the sentence into words
+        words = word_tokenize(sentence)
 
-            # Use NLTK's part-of-speech (POS) tagger to tag each word with its corresponding POS tag
-            pos_tags = nltk.pos_tag(words)
+        # Use NLTK's part-of-speech (POS) tagger to tag each word with its corresponding POS tag
+        pos_tags = nltk.pos_tag(words)
 
-            # Print the tagged words and their POS tags
-            print(pos_tags) 
+        # Print the tagged words and their POS tags
+        print(pos_tags) 
             
     elif choice == "2":
         # code to execute for option 2
